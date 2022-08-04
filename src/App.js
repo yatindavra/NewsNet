a7a0fcf14287e3a9bdb21cc7878b9deb29bdda6d
Yatin <yatindavra@gmail.com>
2022-08-04 10:44:59 +0530

    Changed Into Function Based React App

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-// import logo from './logo.svg';
 import './App.css';
-// bfeac2359e1a49fd9c234322006e5a6a
-import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Navbar from './components/Navbar';
-import News from './components/News';
-// import PropTypes from 'prop-types'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import LoadingBar from 'react-top-loading-bar'
-// import NewsItems from './components/NewsItems';
-const App =()=> {
-const  apiKey = process.env.REACT_APP_NEWS_API  
-const [progress, setProgress] = useState(0)
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
-    return (
-      <div>
+function App() {
+  return (
+    <>
+      <NoteState>
         <Router>
-        <Navbar/>
-        <LoadingBar color='#f11946' height={3} progress={progress}/>
-        <Routes>
-        <Route exact path="/" element={<News setProgress = {setProgress} apiKey={apiKey} key="general" pageSize={9} country="in" category="general"/>}/>
-        <Route exact path="/business" element={<News setProgress = {setProgress} apiKey={apiKey} key="business" pageSize={9} country="in" category="business"/>}/>
-        <Route exact path="/entertainment" element={<News setProgress = {setProgress} apiKey={apiKey} key="entertainment" pageSize={9} country="in" category="entertainment"/>}/>
-        <Route exact path="/general" element={<News setProgress = {setProgress} apiKey={apiKey} key="general" pageSize={9} country="in" category="general"/>}/>
-        <Route exact path="/health" element={<News setProgress = {setProgress} apiKey={apiKey} key="health" pageSize={9} country="in" category="health"/>}/>
-        <Route exact path="/science" element={<News setProgress = {setProgress} apiKey={apiKey} key="science" pageSize={9} country="in" category="science"/>}/>
-        <Route exact path="/sports" element={<News setProgress = {setProgress} apiKey={apiKey} key="sports" pageSize={9} country="in" category="sports"/>}/>
-        <Route exact path="/technology" element={<News setProgress = {setProgress} apiKey={apiKey} key="technology" pageSize={9} country="in" category="technology"/>}/>
-        </Routes>    
-        </Router>      
-      </div>
-    )
-  
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
 }
-
-
-// business
-// entertainment
-// general
-// health
-// science
-// sports
-// technology
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types'
+import { Link, useLocation } from "react-router-dom";
 
-const Navbar =()=> {
+  
+
+const Navbar = () => {
+    let location = useLocation();
     return (
-      <div>
-        <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark" style={{backgroundColor:"white"}}>
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NewsNet</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">Home</Link>
-        </li>
-        {/* <li className="nav-item"><Link className="nav-link" to="/about">About</Link>
-        </li> */}
-        <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-        {/* <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" to="#">Action</a></li>
-            <li><a className="dropdown-item" to="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" to="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-      </ul>
-    </div>
-  </div>
-</nav>
-      </div>
-    )
-  }
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">iNotebook</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">About</Link>
+                        </li>
 
+                    </ul>
+                    <form className="d-flex"> 
+                    <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+                    <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar
